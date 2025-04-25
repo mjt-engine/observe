5f142805aa0e58fd61c638d92c1bb01ba4f1cf0e
Matt Taylor <matt@matthewjosephtaylor.com>
2025-04-25 12:00:16 -0500

    export LogEntry type

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+export * from "./type/TraceId";
+export * from "./type/LogEntry";
+
 export * from "./observe/Observe";
 export * from "./agent/ObserveAgent";
-export * from "./type/TraceId";
 export * from "./agent/Stats";
 export * from "./agent/Timer";
